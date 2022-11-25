9af9c1e3dace4c48fed9f2e41b6715935a6c0c22
Rohit Sharma <106882135+roh972012@users.noreply.github.com>
2022-11-25 23:58:12 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+	<div class="col-sm-12">
+		<p class="back-link">&copy; <?php echo date("Y"); ?> - Developed By Rohit Priyansh Deepanshu Nilay </p>
+	</div>
+</footer>
---
+++ includes/sidebar.php
@@ -0,0 +1,35 @@
+<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+		<div class="profile-sidebar">
+			<div class="profile-userpic">
+				<img src="https://www.w3schools.com/howto/img_avatar.png" width="50" class="img-responsive" alt="Icon">
+			</div>
+			<div class="profile-usertitle">
+				<div class="profile-usertitle-name">Admin</div>
+				<div class="profile-usertitle-status"><span class="indicator label-success"></span>Online</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="divider"></div>
+
+		<form role="search" action="search-results.php" name="search" method="POST" enctype="multipart/form-data">
+
+			<!--  -->
+
+			<div class="form-group">
+				<input type="text" class="form-control" id="searchdata" name="searchdata" placeholder="Search Vehicle-Reg">
+			</div>
+
+		</form>
+		<ul class="nav menu">
+			<li class="<?php if($page=="dashboard") {echo "active";}?>"><a href="dashboard.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
+			<li class="<?php if($page=="vehicle-category") {echo "active";}?>"><a href="vehicle-category.php"><em class="fa fa-th-large">&nbsp;</em> Vehicle Category</a></li>
+			<li class="<?php if($page=="manage-vehicles") {echo "active";}?>"><a href="manage-vehicles.php"><em class="fa fa-car">&nbsp;</em> Vehicle Entry</a></li>
+			<li class="<?php if($page=="in-vehicle") {echo "active";}?>"><a href="in-vehicles.php"><em class="fa fa-toggle-on">&nbsp;</em> IN Vehicles</a></li>
+            <li class="<?php if($page=="out-vehicle") {echo "active";}?>"><a href="out-vehicles.php"><em class="fa fa-toggle-off">&nbsp;</em> OUT Vehicles</a></li>
+			<li class="<?php if($page=="reports") {echo "active";}?>"><a href="reports.php"><em class="fa fa-file">&nbsp;</em> View Reports</a></li>
+			<!-- <li class="<?php if($page=="about") {echo "active";}?>"><a href="about.php"><em class="fa fa-info">&nbsp;</em> About Page</a></li> -->
+			<li class="<?php if($page=="income") {echo "active";}?>"><a href="total-income.php"><em class="fa fa-dollar">&nbsp;</em> Total Income</a></li>
+			
+		</ul>
+		
+	</div><!--/.sidebar-->
